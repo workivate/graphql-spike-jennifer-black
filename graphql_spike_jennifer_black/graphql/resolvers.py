58406d73037ec26cbf40f1ce7ecd023e8feb9367
Jennifer Black <JenniLBlack@outlook.com>
2021-10-04 10:45:50 +0100

    Add search API client

--- graphql_spike_jennifer_black/graphql/resolvers.py
+++ graphql_spike_jennifer_black/graphql/resolvers.py
@@ -1,12 +1,28 @@
+import json
 from logging import Logger
 from typing import Any
 
 from kink import inject
 
+from graphql_spike_jennifer_black.graphql.clients import SearchApiClient
+from graphql_spike_jennifer_black.graphql.dto import SearchRequest
+
 
 @inject
-def get_search_resolver(obj: Any, info: Any, logging: Logger) -> dict:
-    logging.info(info)
-    logging.info(obj)
-    response = {"success": True, "results": ["It's working so far!"]}
+def get_search_resolver(
+    obj: Any,
+    info: Any,
+    query: str,
+    logging: Logger,
+    search_client: SearchApiClient,
+    limit: int = 10,
+    cursor: str = None,
+) -> dict:
+    logging.info(query)
+    search_request = SearchRequest(query=query, limit=limit, cursor=None)
+    if cursor is not None:
+        search_request.cursor = cursor
+    json_results = search_client.get_search_results(search_request)
+    logging.debug(json_results)
+    response = {"success": True, "results": json.dumps(json_results)}
     return response
